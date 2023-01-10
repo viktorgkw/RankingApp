a1cf3e2a41c145673cba4934164f6c3822e9e7c8
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-01-10 14:13:56 +0200

    Add project files.

---
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,56 @@
+﻿import React, { useState, useEffect } from 'react';
+import MovieImageArr from './MovieImages.js'
+import RankingGrid from './RankingGrid.js';
+import ItemCollection from './ItemCollection.js';
+
+const RankItems = () => {
+    const [items, setItems] = useState([]);
+    const dataType = 1;
+
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+
+        const targetElement = ev.target;
+
+        if (targetElement.nodeName === "IMG") {
+            return false;
+        }
+        if (targetElement.childNodes.length === 0) {
+            var data = parseInt(ev.dataTransfer.getData("text").substring(5));
+            const transformedCollection = items.map(item => (item.id === parseInt(data))
+                ? { ...item, ranking: parseInt(targetElement.id.substring(5)) }
+                : { ...item, ranking: item.ranking });
+
+            setItems(transformedCollection);
+        }
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then(results => {
+                return results.json();
+            })
+            .then(data => {
+                setItems(data);
+            })
+    }, []);
+
+    return (
+        (items != null) ?
+            <main>
+                <RankingGrid items={items} imgArr={MovieImageArr} drag={drag} allowDrop={allowDrop} drop={drop} />
+                <ItemCollection items={items} drag={drag} imgArr={MovieImageArr} />
+            </main>
+            : <main>Loading...</main>
+    )
+}
+
+export default RankItems;
---
+++ RankingApp/ClientApp/src/components/RankingGrid.js
@@ -0,0 +1,85 @@
+﻿const RankingGrid = ({ items, imgArr, drag, allowDrop, drop }) => {
+
+    const rankingGrid = [];
+    const cellCollectionInsane = [];
+    const cellCollectionGood = [];
+    const cellCollectionDecent = [];
+    const cellCollectionWorst = [];
+
+    function pushCellMarkupToArray(cellCollection, rankNum, rowLabel) {
+        if (rankNum > 0) {
+            let item = items.find(i => i.ranking === rankNum);
+            cellCollection.push(<div id={`rank-${rankNum}`} onDrop={drop} onDragOver={allowDrop} className="rank-cell">
+                {(item == null)
+                    ? null
+                    : <img id={`item-${item.id}`} src={imgArr.find(i => i.id === item.imageId)?.image} draggable="true" onDragStart={drag} />}
+            </div>);
+        }
+        else {
+            cellCollection.push(<div className="row-label">
+                <h4>{rowLabel}</h4>
+            </div>);
+        }
+    }
+
+    function createCellsForRow(row) {
+        let rankNum = 0;
+        let currentCollection = [];
+        let label = "";
+        const cellsCount = 5;
+
+        for (let a = 1; a <= cellsCount; a++) {
+            rankNum = (a === 1) ? 0 : (cellsCount * (row - 1)) + a - row;
+
+            if (row === 1) {
+                currentCollection = cellCollectionInsane;
+                label = "Insane";
+            }
+            else if (row === 2) {
+                currentCollection = cellCollectionGood;
+                label = "Good";
+            }
+            else if (row === 3) {
+                currentCollection = cellCollectionDecent;
+                label = "Decent";
+            }
+            else if (row === 4) {
+                currentCollection = cellCollectionWorst;
+                label = "Worst";
+            }
+
+            pushCellMarkupToArray(currentCollection, rankNum, label);
+        }
+    }
+
+    function createCellsForRows() {
+        const rowsCount = 4;
+
+        for (let row = 1; row <= rowsCount; row++) {
+            createCellsForRow(row);
+        }
+    }
+
+    function createRowsForGrid() {
+        rankingGrid.push(<div className="rank-row insane-tier">{cellCollectionInsane}</div>);
+        rankingGrid.push(<div className="rank-row good-tier">{cellCollectionGood}</div>);
+        rankingGrid.push(<div className="rank-row decent-tier">{cellCollectionDecent}</div>);
+        rankingGrid.push(<div className="rank-row worst-tier">{cellCollectionWorst}</div>);
+
+        return rankingGrid;
+    }
+
+    function createRankingGrid() {
+        createCellsForRows();
+
+        return createRowsForGrid();
+    }
+
+    return (
+        <div className="rankings">
+            {createRankingGrid()}
+        </div>
+    );
+}
+
+export default RankingGrid;
